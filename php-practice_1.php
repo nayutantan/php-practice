618bcb7ae620e49021c363a81ecc0835219aa938
nayutantan <rorushinamon5@gmail.com>
2025-01-25 16:59:18 +0900

    修正①

--- php-practice_1.php
+++ php-practice_1.php
@@ -1,6 +1,6 @@
 <?php
 // Q1 変数と文字列
-$name='安藤';
+$name = '安藤';
 var_dump($name);
 
 $newMessage = '私は「 ' . $name . ' 」です。';
@@ -8,7 +8,7 @@ var_dump($newMessage);
 
 
 // Q2 四則演算
-$num=5*4;
+$num = 5 * 4;
 var_dump($num);
 
 $num /= 2;
@@ -16,9 +16,8 @@ var_dump($num);
 
 
 // Q3 日付操作
-date_default_timezone_set("Asia/Tokyo");
-$date = new DateTime('now');
-echo $date->format('現在時刻は、Y年m月d日 H時i分s秒です。');
+date_default_timezone_set('Asia/Tokyo');
+echo date('現在時刻は、Y年m月d日 H時i分s秒です。');
 
 
 // Q4 条件分岐-1 if文
@@ -31,7 +30,7 @@ if($device === 'windows') {
     echo '使用OSは、macです。';
 //mac」だったら、「使用OSは、macです。」
 }else {
-    echo'どちらでもありません。';
+    echo 'どちらでもありません。';
 }
 //上記2つ以外だったら、「どちらでもありません。
 
@@ -44,36 +43,36 @@ echo $message;
 
 
 // Q6 配列
-$tokyoarea = ['東京都', '埼玉県', '群馬県','栃木県','千葉県','茨城県','神奈川県'];
-echo ''.$tokyoarea[3].'と'.$tokyoarea[4].'は関東地方の都道府県です。';
+$tokyoarea = ['東京都' , '埼玉県' , '群馬県' , '栃木県' , '千葉県' , '茨城県' , '神奈川県'];
+echo  $tokyoarea[3] . 'と' . $tokyoarea[4] . 'は関東地方の都道府県です。';
 
 
 // Q7 連想配列-1
-$tokyoarea = ['東京都' => '新宿区', '埼玉県' => 'さいたま市', '群馬県' => '前橋市','栃木県' => '宇都宮市','千葉県' => '千葉市','茨城県' => '水戸市','神奈川県' => '横浜市'];
+$tokyoarea = ['東京都' => '新宿区', '埼玉県' => 'さいたま市', '群馬県' => '前橋市', '栃木県' => '宇都宮市', '千葉県' => '千葉市', '茨城県' => '水戸市', '神奈川県' => '横浜市'];
 
-foreach ($tokyoarea as $x => $y) {
-    echo $y."\n";
+foreach ($tokyoarea as $prefecture => $city) {
+    echo $city . "\n";
 }
 
 
 // Q8 連想配列-2
-$tokyoarea = ['東京都' => '新宿区', '埼玉県' => 'さいたま市', '群馬県' => '前橋市','栃木県' => '宇都宮市','千葉県' => '千葉市','茨城県' => '水戸市','神奈川県' => '横浜市'];
+$tokyoarea = ['東京都' => '新宿区', '埼玉県' => 'さいたま市', '群馬県' => '前橋市', '栃木県' => '宇都宮市', '千葉県' => '千葉市', '茨城県' => '水戸市', '神奈川県' => '横浜市'];
 
-foreach ($tokyoarea as $x => $y) {
-    if($x === '埼玉県'){
-        echo $x.'の県庁所在地は、'.$y.'です。';
+foreach ($tokyoarea as $prefecture => $city) {
+    if($prefecture === '埼玉県'){
+        echo $prefecture . 'の県庁所在地は、' . $city . 'です。';
     }
 }
 
 
 // Q9 連想配列-3
-$tokyoarea = ['東京都' => '新宿区', '埼玉県' => 'さいたま市', '群馬県' => '前橋市','栃木県' => '宇都宮市','千葉県' => '千葉市','茨城県' => '水戸市','神奈川県' => '横浜市','新潟県' => '新潟市','愛知県' => '名古屋市'];
+$tokyoarea = ['東京都' => '新宿区', '埼玉県' => 'さいたま市', '群馬県' => '前橋市', '栃木県' => '宇都宮市', '千葉県' => '千葉市', '茨城県' => '水戸市', '神奈川県' => '横浜市', '新潟県' => '新潟市', '愛知県' => '名古屋市'];
 
-foreach ($tokyoarea as $x => $y) {
-    if($x ==='東京都'|| $x==='埼玉県'||$x==='群馬県'||$x==='栃木県'||$x==='千葉県'||$x==='茨城県'||$x==='神奈川県'){
-        echo $x.'の県庁所在地は、'.$y.'です。'."\n";
+foreach ($tokyoarea as $prefecture => $city) {
+    if($prefecture === '東京都' || $prefecture==='埼玉県' || $prefecture === '群馬県' || $prefecture === '栃木県' || $prefecture === '千葉県' || $prefecture === '茨城県' || $prefecture === '神奈川県'){
+        echo $prefecture . 'の県庁所在地は、' . $$city . 'です。' . "\n";
     }else{
-        echo $x.'は関東地方ではありません。'."\n";
+        echo $prefecture . 'は関東地方ではありません。' . "\n";
     }
 }
 
@@ -81,7 +80,7 @@ foreach ($tokyoarea as $x => $y) {
 // Q10 関数-1
 function hello($name)
   {
-    echo $name.'さん、こんにちは。'."\n";
+    echo $name . 'さん、こんにちは。' . "\n";
   }
 
 hello('金谷');
@@ -91,7 +90,7 @@ hello('安藤');
 // Q11 関数-2
 function calcTaxInPrice($price) {
   $taxInPrice = $price * 1.1;
-  echo $price.'の商品の税込価格は'.$taxInPrice. '円です。';
+  echo $price . 'の商品の税込価格は' . $taxInPrice . '円です。';
 }
 
 calcTaxInPrice(1000);
@@ -100,15 +99,15 @@ calcTaxInPrice(1000);
 // Q12 関数とif文
 function distinguishNum($number){
   if($number % 2 === 0) {
-    echo $number.'は偶数です。'."\n";
+    return $number . 'は偶数です。' . "\n";
   }
   else{
-    echo $number.'は奇数です。'."\n";
+    return $number . 'は奇数です。' . "\n";
   }
 }
 
-distinguishNum(11);
-distinguishNum(24);
+echo distinguishNum(11);
+echo distinguishNum(24);
 
 
 // Q13 関数とswitch文
@@ -116,19 +115,19 @@ function evaluateGrade($grade){
   switch ($grade) {
      case 'A':
      case 'B':  // caseは複数まとめて書くことも可能
-         echo '合格です。'."\n";
+         echo '合格です。' . "\n";
          break;
  
      case 'C':
-         echo '合格ですが追加課題があります。'."\n";
+         echo '合格ですが追加課題があります。' . "\n";
          break;
          
      case 'D':
-         echo '不合格です。'."\n";
+         echo '不合格です。' . "\n";
          break;
  
      default:
-         echo '判定不明です。講師に問い合わせてください。'."\n";
+         echo '判定不明です。講師に問い合わせてください。' . "\n";
          // 条件が全てfalseだった時の処理
          break;
      }
