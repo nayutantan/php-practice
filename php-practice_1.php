6c379910d1a3f5b3393224f776923c1b98535a11
nayutantan <rorushinamon5@gmail.com>
2025-02-25 18:32:41 +0900

    php練習問題➀のq13を返り値で求めるよう修正

--- php-practice_1.php
+++ php-practice_1.php
@@ -111,25 +111,25 @@ function evaluateGrade($grade){
   switch ($grade) {
      case 'A':
      case 'B':  // caseは複数まとめて書くことも可能
-         echo '合格です。' . "\n";
+         return '合格です。' . "\n";
          break;
  
      case 'C':
-         echo '合格ですが追加課題があります。' . "\n";
+         return '合格ですが追加課題があります。' . "\n";
          break;
          
      case 'D':
-         echo '不合格です。' . "\n";
+         return '不合格です。' . "\n";
          break;
  
      default:
-         echo '判定不明です。講師に問い合わせてください。' . "\n";
+         return '判定不明です。講師に問い合わせてください。' . "\n";
          // 条件が全てfalseだった時の処理
          break;
      }
  }
  
- evaluateGrade('A');
- evaluateGrade('E');
+ echo evaluateGrade('A');
+ echo evaluateGrade('E');
 
 ?>
